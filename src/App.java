6bde1b3fd81b57a7e724aa279ca9c0f43b043295
reixhons <xhonsigjoni@gmail.com>
2025-03-20 09:48:32 +0100

    Read & Print File Test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String filePath = "src/file.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
     }
 }
